5fb28c4a80ee6f380a87d4d9f6e6679489a71847
aca98aca <ahmetcelil98@gmail.com>
2025-05-23 16:00:20 +0300

    Remove config.py with API key and add config.template.py

--- config.py
+++
@@ -1,2 +0,0 @@
-# Alpha Vantage API Configuration
-ALPHA_VANTAGE_API_KEY = "76X8R4NJ4U8T2DQL"  # Alpha Vantage API key 
---
+++ config.template.py
@@ -0,0 +1,2 @@
+# Alpha Vantage API Configuration
+ALPHA_VANTAGE_API_KEY = "YOUR_API_KEY_HERE"  # Replace with your Alpha Vantage API key 
